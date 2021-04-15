8338ac3741abc3f7345000dc5b69313d43cee469
joeljiezhu <joelckchu@outlook.com>
2021-04-15 14:42:37 +0800

    something seriously wrong with the system

--- mean/database/run.sh
+++ mean/database/run.sh
@@ -1,3 +1,4 @@
 # run the mongod with <db-dir>
+# @NOTE we are running podman here NOT docker
 
-docker run -d -p 27017:27017 -v ./:/data/db --name mongodb docker.io/library/mongo
+podman run -d -p 27017:27017 -v ./:/data/db --name mongodb docker.io/library/mongo
