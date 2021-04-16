22bdeaf65669789125eab64d97ecb0563b762eae
joeljiezhu <joelckchu@outlook.com>
2021-04-16 14:23:06 +0800

    commit the mean code before continue with the python stuff

--- mean/express/index.js
+++ mean/express/index.js
@@ -1,3 +1,12 @@
 // the top level file to run the express app
 const express = require('express')
-const http = require('http')
+// const http = require('http')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send(`Hello`)
+})
+.listen(PORT, () => {
+  console.log(`Express is running on ${PORT}`)
+})
