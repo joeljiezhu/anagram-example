c37eb8d66bbba412b554437455dd902c8fd6056b
joeljiezhu <joelckchu@outlook.com>
2021-04-23 20:33:26 +0800

    mean dev continue later

--- mean/mongo/init.js
+++ mean/mongo/init.js
@@ -1,1 +1,2 @@
 // Everytime the app start up we run this init script to pull the text files into the mongodb
+// @TODO 
--- mean/mongo/run.sh
+++ mean/mongo/run.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
+
 # run the mongod with <db-dir>
 # @NOTE we are running podman here NOT docker
-
-podman run -d -p 27017:27017 -v ./:/data/db --name mongodb docker.io/library/mongo
+# we use podman instead SAME just replace docker with podman 
+docker run -d -p 27017:27017 -v $1:/data/db --name mongodb docker.io/library/mongo
